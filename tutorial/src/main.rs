a7555e10ea3e1464bd4b78282fad125803b57d16
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2025-03-03 11:47:57 +0200

    Game state + default

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -2,13 +2,29 @@ use rusty_engine::prelude::*;
 
 #[derive(Resource)]
 struct GameState {
-   health_left: i32,
+   high_score: u32,
+   current_score: u32,
+   enemy_labels: Vec<String>,
+   spawn_timer: Timer,
 }
+
+impl Default for GameState {
+   fn default() -> Self {
+      Self {
+         high_score: 0,
+         current_score: 0,
+         enemy_labels: Vec::new(),
+         spawn_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
+      }
+   }
+}
+
+
 
 fn main() {
    let mut game = Game::new();
 
    // get your game stuff ready here
 
-   game.run(GameState { health_left: 42 });
+   game.run(GameState::default());
 }
