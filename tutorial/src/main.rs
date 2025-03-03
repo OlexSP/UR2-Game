2b0bb4126d96cd50530c93d0282da7c43fa8c5ac
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2025-03-03 15:41:35 +0200

    colliders

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, unused_variables)]
+
 use rusty_engine::prelude::*;
 
 #[derive(Resource)]
@@ -23,21 +25,27 @@ impl Default for GameState {
 
 fn main() {
    let mut game = Game::new();
+   game.show_colliders = true;
 
-   let player = game.add_sprite("my_player", SpritePreset::RacingCarBlue);
-   player.translation = Vec2::new(200.0, 100.0); // Move the car up and to the right
-   player.rotation = UP; // UP is one of the built-in constants you can use
-   player.scale = 2.5; // It's a BIG car!
-   player.layer = CHARACTER_LAYER; // as in previous code snippet
-
-
-   // get your game stuff ready here
    let game_state = GameState::default();
+
+   let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
+   player.translation = Vec2::new(0.0, 0.0);
+   player.rotation = SOUTH_WEST;
+   player.scale = 1.0;
+   player.layer = CHARACTER_LAYER;
+   player.collision = true;
+
+   let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
+   car1.translation = Vec2::new(300.0, 0.0);
+   car1.layer = CHARACTER_LAYER;
+   car1.collision = true;
 
    game.add_logic(game_logic); // Don't forget to add the logic function to the game!
    game.run(game_state);
 }
 
+// Layers
 const BACKGROUND_LAYER: f32 = 0.0;
 const CHARACTER_LAYER: f32 = 1.0;
 const EFFECTS_LAYER: f32 = 2.0;
@@ -45,6 +53,19 @@ const UI_BOTTOM_LAYER: f32 = 3.0;
 const UI_TOP_LAYER: f32 = 4.0;
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-   // game_state.current_score += 1;
-   // println!("Current score: {}", game_state.current_score);
+   for event in engine.collision_events.drain(..) {
+      if event.state == CollisionState::Begin && event.pair.one_starts_with("player"){
+         // remove the sprite the player is colliding with
+         for label in [event.pair.0, event.pair.1] {
+             if label != "player" {
+                 engine.sprites.remove(&label);
+             }
+         }
+          game_state.current_score += 1;
+          println!("Current score: {}", game_state.current_score);
+      }
+   };
+
+   let player = engine.sprites.get_mut("player").unwrap();
+   player.translation.x += 100.0 * engine.delta_f32;
 }
