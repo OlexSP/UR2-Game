56b3277f0c9407d93c64c38cbde67798304b6cc7
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2025-03-04 14:07:07 +0200

    text plates

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -5,8 +5,8 @@ use rusty_engine::prelude::KeyCode::{Down, Left, Right, Up, A, D, S, W};
 
 #[derive(Resource)]
 struct GameState {
-   // high_score: u32,
-   current_score: u32,
+   high_score: u32,
+   score: u32,
    ferris_index: i32,
    // enemy_labels: Vec<String>,
    // spawn_timer: Timer,
@@ -15,8 +15,8 @@ struct GameState {
 impl Default for GameState {
    fn default() -> Self {
       Self {
-         // high_score: 0,
-         current_score: 0,
+         high_score: 0,
+         score: 0,
          ferris_index: 0,
          // enemy_labels: Vec::new(),
          // spawn_timer: Timer::from_seconds(1.0, TimerMode::Once),
@@ -28,10 +28,11 @@ impl Default for GameState {
 
 fn main() {
    let mut game = Game::new();
-   game.show_colliders = true;
+   game.show_colliders = false;
 
    let game_state = GameState::default();
 
+   // Add the player car
    let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
    player.translation = Vec2::new(0.0, 0.0);
    player.rotation = SOUTH_WEST;
@@ -39,10 +40,20 @@ fn main() {
    player.layer = CHARACTER_LAYER;
    player.collision = true;
 
-   let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
-   car1.translation = Vec2::new(300.0, 0.0);
-   car1.layer = CHARACTER_LAYER;
-   car1.collision = true;
+   // let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
+   // car1.translation = Vec2::new(300.0, 0.0);
+   // car1.layer = CHARACTER_LAYER;
+   // car1.collision = true;
+
+   // Add text plate
+   let score = game.add_text("score", "Score: 0");
+   score.translation = Vec2::new(520.0, 320.0);
+   score.layer = UI_TOP_LAYER;
+
+   let high_score = game.add_text("high_score", "High score: 0");
+   high_score.translation = Vec2::new(-520.0, 320.0);
+   high_score.layer = UI_TOP_LAYER;
+
 
    game.add_logic(game_logic); // Don't forget to add the logic function to the game!
    game.run(game_state);
@@ -65,13 +76,20 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
                  engine.sprites.remove(&label);
              }
          }
-          game_state.current_score += 1;
-          println!("Current score: {}", game_state.current_score);
+         game_state.score += 1;
+         // change the text plate
+         let score_text = engine.texts.get_mut("score").unwrap();
+         score_text.value = format!("Score: {}", game_state.score);
+         if game_state.score > game_state.high_score {
+            game_state.high_score = game_state.score;
+            let high_score_text = engine.texts.get_mut("high_score").unwrap();
+            high_score_text.value = format!("High score: {}", game_state.high_score);
+         }
       }
    };
    // handle movements
    let player = engine.sprites.get_mut("player").unwrap();
-   const MOVEMENT_SPEED: f32 = 100.0;
+   const MOVEMENT_SPEED: f32 = 200.0;
    if engine.keyboard_state.pressed_any(&[Up, W]) {
       player.translation.y += MOVEMENT_SPEED * engine.delta_f32;
    }
@@ -94,6 +112,13 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
          ferris.translation = mouse_location;
          ferris.collision = true;
       }
+   }
+
+   // reset score
+   if engine.keyboard_state.just_pressed(KeyCode::R) {
+      game_state.score = 0;
+      let score_text = engine.texts.get_mut("score").unwrap();
+      score_text.value = format!("Score: {}", game_state.score);
    }
 
 }
