65600bc9299776310f9d51ba5efde25aebf6b5ad
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2025-03-06 10:02:15 +0200

    game common setup

---
+++ game/src/main.rs
@@ -0,0 +1,17 @@
+use rusty_engine::prelude::*;
+
+#[derive(Resource)]
+struct GameState {}
+
+fn main() {
+    let mut game = Game::new();
+
+    // game setup goes here
+
+    game.add_logic(game_logic);
+    game.run(GameState {});
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    // game logic goes here
+}
