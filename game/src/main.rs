68ce4eb96998003123b804609e8091e870c2cd00
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2025-03-06 10:34:39 +0200

    create player sprite + background music

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,17 +1,39 @@
+#![allow(dead_code, unused_variables)]
 use rusty_engine::prelude::*;
 
 #[derive(Resource)]
-struct GameState {}
+struct GameState {
+    health_amount: u8,
+    lost: bool,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            health_amount: 5,
+            lost: false,
+        }
+    }
+}
 
 fn main() {
     let mut game = Game::new();
 
-    // game setup goes here
+    // music
+    game.audio_manager.play_music(MusicPreset::WhimsicalPopsicle, 0.2);
+
+    // add sprite
+    let player1 = game.add_sprite("player1", SpritePreset::RacingCarBlue);
+    player1.translation = Vec2::new(-500.0, 0.0);
+    player1.layer = 10.0;
+    player1.collision = true;
+
 
     game.add_logic(game_logic);
-    game.run(GameState {});
+    game.run(GameState::default());
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
     // game logic goes here
+    let player1 = engine.sprites.get_mut("player1").unwrap();
 }
