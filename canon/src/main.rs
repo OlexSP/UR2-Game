0a6a01f02f71b29edc90442100cc2c40e9bf4ccf
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2025-03-10 12:50:10 +0200

    game initialization

--- canon/src/main.rs
+++ canon/src/main.rs
@@ -12,8 +12,8 @@ struct GameState {
 impl Default for GameState {
     fn default() -> Self {
         Self {
-            magnitude: 0.0,
-            rotation: 0.0,
+            magnitude: 10.0,
+            rotation: 1.25,
             ball_velocity: Vec2::ZERO,
         }
     }
@@ -21,6 +21,7 @@ impl Default for GameState {
 const GRAVITY_ACCELERATION: f32 = -9.81;
 const BALL_LAYER: f32 = 1.0;
 const CANON_LAYER: f32 = 2.0;
+const TEXT_LAYER: f32 = 3.0;
 
 fn main() {
     let mut game = Game::new();
@@ -32,13 +33,13 @@ fn main() {
     ball.layer = BALL_LAYER;
 
     let canon = game.add_sprite("canon", SpritePreset::RacingBarrierRed);
-    canon.translation = Vec2::new(0.0, -355.0);
-    canon.rotation = NORTH;
+    canon.translation = Vec2::new(-600.0, -355.0);
+    canon.rotation = game_state.rotation;
     canon.scale = 0.5;
     canon.layer = CANON_LAYER;
 
     let goal = game.add_sprite("goal", SpritePreset::RacingConeStraight);
-    goal.translation = Vec2::new(0.0, 200.0);
+    goal.translation = Vec2::new(400.0, -320.0);
     goal.scale = 1.5;
     goal.layer = CANON_LAYER;
 
@@ -50,13 +51,17 @@ fn main() {
 
     for (i, preset) in obstacles.into_iter().enumerate() {
         let obstacle = game.add_sprite(format!("obstacle{}", i), preset);
-        obstacle.translation = Vec2::new(-100.0 + i as f32 * 100.0, 0.0 );
-        if i == 1 {
-            obstacle.scale = 0.5;
-        }
+        obstacle.translation = Vec2::new(-100.0 + i as f32 * 100.0, -300.0 );
         obstacle.collision = true;
     }
 
+    // text
+    let magnitude_text = game.add_text("magnitude", format!("Magnitude: {}", game_state.magnitude));
+    magnitude_text.translation = Vec2::new(-540.0, 320.0);
+    magnitude_text.layer = TEXT_LAYER;
+
+    // music
+    game.audio_manager.play_music(MusicPreset::WhimsicalPopsicle, 0.2);
 
     game.add_logic(game_logic);
     game.run(game_state);
