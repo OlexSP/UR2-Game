f0ee8a312e37f5db68dfa1640e761f0bf4266fcc
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2025-03-11 12:50:40 +0200

    + random obstacle transl&rot + random goal translation game logic + detect ball collisions + change sprites' translation

--- canon/src/main.rs
+++ canon/src/main.rs
@@ -1,6 +1,9 @@
 #![allow(dead_code, unused_variables)]
 
+use std::f64::consts::PI;
 use rusty_engine::prelude::*;
+use rand::prelude::*;
+use rand::rng;
 
 #[derive(Resource)]
 struct GameState {
@@ -43,9 +46,13 @@ fn main() {
     cannon_wheel.layer = CANON_LAYER + 1.0;
 
     let goal = game.add_sprite("goal", SpritePreset::RacingConeStraight);
-    goal.translation = Vec2::new(400.0, -320.0);
+    goal.translation = Vec2::new(
+        rng().random_range(250.0..600.0),
+        rng().random_range(-330.0..300.0)
+    );
     goal.scale = 1.5;
     goal.layer = CANON_LAYER;
+    goal.collision = true;
 
     let obstacles = vec![
         SpritePreset::RollingBlockSquare,
@@ -55,7 +62,11 @@ fn main() {
 
     for (i, preset) in obstacles.into_iter().enumerate() {
         let obstacle = game.add_sprite(format!("obstacle{}", i), preset);
-        obstacle.translation = Vec2::new(-100.0 + i as f32 * 100.0, -300.0);
+        obstacle.translation = Vec2::new(
+            rng().random_range(-250.0..250.0),
+            rng().random_range(-320.0..320.0)
+        );
+        obstacle.rotation = rng().random_range(0.0..2.0 * PI as f32);
         obstacle.collision = true;
     }
 
@@ -128,24 +139,59 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
         game_state.ball_velocity.x = game_state.ball_velocity.x.clamp(0.0, 1000.0);
 
         if canon_ball.translation.x > 750.0
-            || canon_ball.translation.y > 400.0
+            || canon_ball.translation.y > 1000.0
             || canon_ball.translation.y < -400.0
         {
             engine.sprites.remove("ball");
         }
     } else if engine.keyboard_state.just_pressed(KeyCode::Space)
-        || engine.mouse_state.just_pressed(MouseButton::Left)
-    {
-        let cannon_ball = engine.add_sprite("ball", SpritePreset::RollingBallRed);
-        cannon_ball.translation = c_translation;
-        cannon_ball.rotation = c_rotation;
-        cannon_ball.layer = BALL_LAYER;
+        || engine.mouse_state.just_pressed(MouseButton::Left) {
+            let cannon_ball = engine.add_sprite("ball", SpritePreset::RollingBallRed);
+            cannon_ball.translation = c_translation;
+            cannon_ball.rotation = c_rotation;
+            cannon_ball.layer = BALL_LAYER;
+            cannon_ball.collision = true;
 
-        game_state.ball_velocity = Vec2::new(
-            game_state.magnitude * MAGNITUDE_MULTIPLIER * c_rotation.cos(),
-            game_state.magnitude * MAGNITUDE_MULTIPLIER * c_rotation.sin(),
-        );
+            game_state.ball_velocity = Vec2::new(
+                game_state.magnitude * MAGNITUDE_MULTIPLIER * c_rotation.cos(),
+                game_state.magnitude * MAGNITUDE_MULTIPLIER * c_rotation.sin(),
+            );
 
-        engine.audio_manager.play_sfx(SfxPreset::Click, 0.2);
+            engine.audio_manager.play_sfx(SfxPreset::Click, 0.2);
+        }
+
+    // handle collisions
+    for event in engine.collision_events.drain(..) {
+        if !event.pair.either_contains("ball") || event.state.is_end() { continue; }
+            for label in [event.pair.0, event.pair.1] {
+                // remove the ball if it hits an obstacle
+                if label.starts_with("obstacle") {
+                    engine.sprites.remove("ball");
+                    engine.audio_manager.play_sfx(SfxPreset::Impact2, 0.5);
+                } else if label.starts_with("goal"){
+                    engine.audio_manager.play_sfx(SfxPreset::Impact2, 0.5);
+                    // new goal translation
+                    let goal = engine.sprites.get_mut("goal").unwrap();
+                    goal.translation = Vec2::new(
+                        rng().random_range(250.0..600.0),
+                        rng().random_range(-330.0..300.0)
+                    );
+                    goal.scale = 1.5;
+                    goal.layer = CANON_LAYER;
+                    // new obstacle translations
+                    for sprite in engine.sprites.values_mut() {
+                        if sprite.label.starts_with("obstacle"){
+                            sprite.translation = Vec2::new(
+                                rng().random_range(-250.0..250.0),
+                                rng().random_range(-320.0..320.0)
+                            );
+                            sprite.rotation = rng().random_range(0.0..2.0 * PI as f32);
+                        }
+                    }
+                }
+
+            }
+
+
     }
 }
